7cf6c4845525279ef2e1f4271b8a0968735b0aff
Anil Can <anilcan@macbook-pro.local>
2019-01-28 03:27:16 +0300

    Conver to MVC Design Pattern

---
+++ Todoey/Data Model/Title.swift
@@ -0,0 +1,14 @@
+//
+//  Title.swift
+//  Todoey
+//
+//  Created by Anil Can on 28.01.2019.
+//  Copyright © 2019 Anil Can. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title : String = ""
+    var done : Bool = false
+}
